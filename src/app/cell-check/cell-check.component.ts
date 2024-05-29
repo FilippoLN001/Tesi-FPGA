377d819ec0df043eb5d7fcaaf796c2b017c5ae37
FilippoLN001 <filippo.niculae@gmail.com>
2024-05-29 09:58:43 +0200

    add output counter

--- src/app/cell-check/cell-check.component.ts
+++ src/app/cell-check/cell-check.component.ts
@@ -1,31 +1,30 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
+interface CheckboxState {
+  active: boolean;
+  negated: boolean;
+}
+
 @Component({
   selector: 'app-cell-check',
   templateUrl: './cell-check.component.html',
   styleUrls: ['./cell-check.component.css']
 })
 export class CellCheckComponent {
-  @Input() column: 'and1' | 'or' | 'and2' | undefined; // Colonna della checkbox
-  @Input() index: number | undefined; // Indice della riga
-  @Input() selectedStates: number[] = []; // Stati selezionati delle checkbox
-  @Output() update = new EventEmitter<any>(); // Evento di aggiornamento
-  values: number[] = [];
+  @Input() column!: 'and1' | 'or' | 'and2';
+  @Input() index!: number;
+  @Input() selectedStates: CheckboxState[] = [];
+  @Output() update = new EventEmitter<{ column: 'and1' | 'or' | 'and2', index: number, values: CheckboxState[] }>();
 
-  ngOnInit() {
-    this.values = [...this.selectedStates]; // Inizializza i valori con gli stati selezionati
+  toggleActive(i: number) {
+    const updatedStates = [...this.selectedStates];
+    updatedStates[i] = { ...updatedStates[i], active: !updatedStates[i].active };
+    this.update.emit({ column: this.column, index: this.index, values: updatedStates });
   }
 
-  ngOnChanges() {
-    this.values = [...this.selectedStates]; // Aggiorna i valori quando gli stati cambiano
-  }
-
-  toggleValue(i: number) {
-    if (this.values.includes(i)) {
-      this.values = this.values.filter(v => v !== i); // Rimuove il valore se già presente
-    } else {
-      this.values.push(i); // Aggiunge il valore se non presente
-    }
-    this.update.emit({ column: this.column, index: this.index, values: this.values }); // Emette l'evento di aggiornamento
+  toggleNegated(i: number) {
+    const updatedStates = [...this.selectedStates];
+    updatedStates[i] = { ...updatedStates[i], negated: !updatedStates[i].negated };
+    this.update.emit({ column: this.column, index: this.index, values: updatedStates });
   }
 }
